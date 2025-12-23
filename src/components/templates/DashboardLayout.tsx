1abe40b89c0e9eee3e9f2c38ac8b5d1058c507f1
Chismapary <ismaelg123ra@gmail.com>
2025-12-23 14:07:00 -0500

    Implementación de admin, pasarela de pagos y el boton de notificaciones

--- src/components/templates/DashboardLayout.tsx
+++ src/components/templates/DashboardLayout.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { Outlet } from 'react-router'
 import Nav from '../molecules/dashboard/Nav'
+import NotificationButton from '../molecules/NotificationButton'
+
 const DashboardLayout = () => {
   return (
     <div className='| min-h-screen bg-linear-to-br from-[#0a0e0d] via-[#0d1512] to-[#0a0e0d] flex  '>
         <div className='hidden sm:block border-r-1 border-gray-800'>
-          <div className="p-6 bg-gradient-to-r from-[#006B4B] to-[#00A676]">
-            <h1 className="text-2xl font-bold text-white">Qori<span className="text-[#FFB800]">Preu</span></h1>
-            <p className="text-sm text-white/80 mt-1">Academia Cusco</p>
+          <div className="p-6 bg-gradient-to-r from-[#006B4B] to-[#00A676] flex items-center justify-between">
+            <div>
+              <h1 className="text-2xl font-bold text-white">Qori<span className="text-[#FFB800]">Preu</span></h1>
+              <p className="text-sm text-white/80 mt-1">Academia Cusco</p>
+            </div>
+            <NotificationButton />
           </div>
           <Nav/>
         </div>
